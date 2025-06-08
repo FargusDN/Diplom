f62db017192098ed349659142285c02ed4cd61cf
impoibles <152866979+impoibles@users.noreply.github.com>
2025-06-08 14:51:55 +0300

    Delete AuthorizationService directory

--- AuthorizationService/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseSettings
-from datetime import timedelta
-
-class Settings(BaseSettings):
-    SECRET_KEY: str = "728c774459b57bbb833f4fee54f244dc"
-    ACCESS_TOKEN_EXPIRE: timedelta = timedelta(minutes=30)
-    DATABASE_URL: str = "postgresql://user:password@db:5432/auth_db"
-
-settings = Settings()
