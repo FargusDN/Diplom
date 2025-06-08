9df23fb929d07eb56681fb18d1d4ae6f45c82f9a
Fargus <dimuc2015@mail.ru>
2025-06-08 19:32:01 +0300

    Доработка

--- VUCService/models/postgresqlModel.py
+++ VUCService/models/postgresqlModel.py
@@ -1,79 +1,143 @@
 from datetime import datetime, date
 from typing import List
-
-from sqlalchemy import Column, Integer, String, ForeignKey, Date
-from sqlalchemy.orm import mapped_column, Mapped, relationship
-
+from sqlalchemy import Column, Integer, String, ForeignKey, Date, Float, Boolean
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 from databasePostgres import Base
+# Таблица "PositionsInVUC"
+class PositionsInVUC(Base):
+    __tablename__ = "positions_in_vuc"
+    __table_args__ = {'schema': 'public'}
+    position_in_vuc_id: Mapped[int] = mapped_column(primary_key=True)
+    position_in_vuc_name: Mapped[str] = mapped_column(nullable=False, server_default="Не определено")
+    create_dttm: Mapped[datetime] = mapped_column(default=datetime.now, nullable=False)
+    change_dttm: Mapped[datetime] = mapped_column(nullable=True)
+# Таблица "RoleInNaryad"
+class RoleInNaryad(Base):
+    __tablename__ = "role_in_naryad"
+    __table_args__ = {'schema': 'public'}
+    role_in_naryad_id: Mapped[int] = mapped_column(primary_key=True)
+    role_name_in_naryad: Mapped[str] = mapped_column(nullable=False, server_default="Не определено")
+    create_dttm: Mapped[datetime] = mapped_column(default=datetime.now, nullable=False)
+    change_dttm: Mapped[datetime] = mapped_column(nullable=True)
+    @staticmethod
+    def validate_role_name_in_naryad(value: str):
+        allowed_roles = ['Дежурный', 'Помощник', 'Дневальный']
+        if value not in allowed_roles:
+            raise ValueError(f"role_name_in_naryad must be one of {allowed_roles}")
+# Таблица "VUCUsers"
+class VUCUsers(Base):
+    __tablename__ = "vuc_users"
+    __table_args__ = {'schema': 'public'}
+    vuc_user_id: Mapped[int] = mapped_column(primary_key=True)
+    position_in_vuc_id: Mapped[int] = mapped_column(ForeignKey("public.positions_in_vuc.position_in_vuc_id"), nullable=False)
+    manager_id: Mapped[int] = mapped_column(ForeignKey("public.vuc_users.vuc_user_id"), nullable=True)
+    last_name: Mapped[str] = mapped_column(server_default="Не определено")
+    first_name: Mapped[str] = mapped_column(server_default="Не определено")
+    middle_name: Mapped[str] = mapped_column(server_default="Не определено")
+    birth_dt: Mapped[date] = mapped_column(server_default="1925-01-01")
+    signal_ind: Mapped[str] = mapped_column(nullable=False, server_default="active")
+    create_dttm: Mapped[datetime] = mapped_column(default=datetime.now, nullable=False)
+    change_dttm: Mapped[datetime] = mapped_column(nullable=True)
+    auto_delete_dt: Mapped[date] = mapped_column(default=(datetime.now().date()), nullable=False)
+    manager: Mapped["VUCUsers"] = relationship("VUCUsers", remote_side=[vuc_user_id])
+# Таблица "MilitaryInventory"
+class MilitaryInventory(Base):
+    __tablename__ = "military_inventory"
+    __table_args__ = {'schema': 'public'}
+    id_inventory: Mapped[int] = mapped_column(primary_key=True)
+    name_inventory: Mapped[str] = mapped_column(nullable=False, unique=True)
+    category_inventory: Mapped[str] = mapped_column(nullable=False)
+    quantity_inventory: Mapped[int] = mapped_column(nullable=False)
+    responsible_id: Mapped[int] = mapped_column(ForeignKey("public.vuc_users.vuc_user_id"), nullable=False)
+    last_dt_check_inventory: Mapped[date] = mapped_column(default=datetime.now().date(), nullable=False)
+    create_dttm: Mapped[datetime] = mapped_column(default=datetime.now, nullable=False)
+    change_dttm: Mapped[datetime] = mapped_column(nullable=True)
+# Таблица "InventoryTransactions"
+class InventoryTransactions(Base):
+    __tablename__ = "inventory_transactions"
+    __table_args__ = {'schema': 'public'}
+    id_inventory_transaction: Mapped[int] = mapped_column(primary_key=True)
+    id_inventory: Mapped[int] = mapped_column(ForeignKey("public.military_inventory.id_inventory"), nullable=False)
+    type_transaction: Mapped[str] = mapped_column(nullable=False)
+    amount: Mapped[int] = mapped_column(nullable=False)
+    transaction_date: Mapped[date] = mapped_column(default=datetime.now().date(), nullable=False)
+    signal_ind: Mapped[str] = mapped_column(nullable=False, server_default="active")
+    issuer_id: Mapped[int] = mapped_column(ForeignKey("public.vuc_users.vuc_user_id"), nullable=False)
+    vuc_user_id: Mapped[int] = mapped_column(ForeignKey("public.vuc_users.vuc_user_id"), nullable=False)
+    create_dttm: Mapped[datetime] = mapped_column(default=datetime.now, nullable=False)
+    change_dttm: Mapped[datetime] = mapped_column(nullable=True)
+# Таблица "MilitaryApplications"
+class MilitaryApplications(Base):
+    __tablename__ = "military_applications"
+    __table_args__ = {'schema': 'public'}
+    id_military_application: Mapped[int] = mapped_column(primary_key=True)
+    signal_ind: Mapped[str] = mapped_column(nullable=False, server_default="active")
+    student_id: Mapped[int] = mapped_column(nullable=False)
+    last_name: Mapped[str] = mapped_column(server_default="Не определено")
+    first_name: Mapped[str] = mapped_column(server_default="Не определено")
+    middle_name: Mapped[str] = mapped_column(server_default="Не определено")
+    birth_dt: Mapped[date] = mapped_column(server_default="1925-01-01")
+    study_group_name: Mapped[str] = mapped_column(server_default="Не определено")
+    application_date: Mapped[date] = mapped_column(default=datetime.now().date())
+    status_military_application: Mapped[str] = mapped_column(server_default="В процессе")
+    create_dttm: Mapped[datetime] = mapped_column(default=datetime.now, nullable=False)
+    change_dttm: Mapped[datetime] = mapped_column(nullable=True)
 
+class SelectionProtocols(Base):
+    __tablename__ = "selection_protocols"
+    __table_args__ = {'schema': 'public'}
+    id_protocol: Mapped[int] = mapped_column(primary_key=True)
+    protocol_date: Mapped[date] = mapped_column(default=datetime.now().date())
+    vacancy_count: Mapped[int] = mapped_column(nullable=False)
+    min_rank: Mapped[float] = mapped_column(nullable=False)
+    id_military_application: Mapped[int] = mapped_column(
+        ForeignKey("public.military_applications.id_military_application"), nullable=False)
+    create_dttm: Mapped[datetime] = mapped_column(default=datetime.now, nullable=False)
+    change_dttm: Mapped[datetime] = mapped_column(nullable=True)
+    # Relationship (если нужно)
+    military_application: Mapped["MilitaryApplications"] = relationship(back_populates="selection_protocols")
 
-# Модель пользователя
-class User(Base):
-    __tablename__ = "users"
-    __table_args__ = {'schema': 'partitions'}
-    login_user: Mapped[str] = mapped_column(primary_key=True)
-    password_user: Mapped[str] = mapped_column(nullable=False)
-    role_user: Mapped[str] = mapped_column(nullable=False, server_default="simple_user")
-    info: Mapped["UserInfo"] = relationship()
+# Таблица "PreliminaryResults"
+class PreliminaryResults(Base):
+    __tablename__ = "preliminary_results"
+    __table_args__ = {'schema': 'public'}
+    id_preliminary_result: Mapped[int] = mapped_column(primary_key=True)
+    id_military_application: Mapped[int] = mapped_column(
+        ForeignKey("public.military_applications.id_military_application"), nullable=False)
+    medical_status: Mapped[str] = mapped_column(nullable=False)
+    psych_test_score: Mapped[int] = mapped_column(nullable=False)
+    create_dttm: Mapped[datetime] = mapped_column(default=datetime.now, nullable=False)
+    change_dttm: Mapped[datetime] = mapped_column(nullable=True)
+    # Relationship (если нужно)
+    military_application: Mapped["MilitaryApplications"] = relationship(back_populates="preliminary_results")
 
+# Таблица "MainResults"
+class MainResults(Base):
+    __tablename__ = "main_results"
+    __table_args__ = {'schema': 'public'}
+    id_main_result: Mapped[int] = mapped_column(primary_key=True)
+    id_military_application: Mapped[int] = mapped_column(
+        ForeignKey("public.military_applications.id_military_application"), nullable=False)
+    gpa: Mapped[float] = mapped_column(nullable=False)
+    physical_score: Mapped[int] = mapped_column(nullable=False)
+    total_rank: Mapped[float] = mapped_column(nullable=False)
+    create_dttm: Mapped[datetime] = mapped_column(default=datetime.now, nullable=False)
+    change_dttm: Mapped[datetime] = mapped_column(nullable=True)
+    # Relationship (если нужно)
+    military_application: Mapped["MilitaryApplications"] = relationship(back_populates="main_results")
 
-
-class Institutes(Base):
-    __tablename__ = "institutes"
-    __table_args__ = {'schema': 'partitions'}
-    institute_id: Mapped[int] = mapped_column(primary_key=True)
-    institute_name: Mapped[str] = mapped_column(nullable=False, server_default="Не определено")
-    faculties: Mapped[List["Faculties"]] = relationship()
-
-
-class Faculties(Base):
-    __tablename__ = "faculties"
-    __table_args__ = {'schema': 'partitions'}
-    faculty_id: Mapped[int] = mapped_column(primary_key=True)
-    faculty_code: Mapped[str] = mapped_column(nullable=False, server_default="Не определено")
-    faculty_name: Mapped[str] = mapped_column(nullable=False, server_default="Не определено")
-    institute_id: Mapped[int] = mapped_column(ForeignKey("partitions.institutes.institute_id"))
-    studyGroup: Mapped[List["StudyGroup"]] = relationship()
-
-
-class StudyGroup(Base):
-    __tablename__ = "study_group"
-    __table_args__ = {'schema': 'partitions'}
-    group_id: Mapped[int] = mapped_column(primary_key=True)
-    group_name: Mapped[str] = mapped_column(nullable=False)
-    faculty_id: Mapped[int] = mapped_column(ForeignKey("partitions.faculties.faculty_id"))
-    user_info: Mapped["UserInfo"] = relationship()
-
-class UserInfo(Base):
-    __tablename__ = "user_info"
-    __table_args__ = {'schema': 'partitions'}
-    user_id: Mapped[int] = mapped_column(primary_key=True)
-    user_status: Mapped[str] = mapped_column(nullable=True,server_default="Студент")
-    login_user: Mapped[str] = mapped_column(ForeignKey("partitions.users.login_user"),unique=True)
-    last_name: Mapped[str] = mapped_column(nullable=False, server_default="Не определено")
-    first_name: Mapped[str] = mapped_column(nullable=False, server_default="Не определено")
-    middle_name: Mapped[str] = mapped_column(nullable=False, server_default="Не определено")
-    birth_dt: Mapped[date] = mapped_column(nullable=False,server_default="1925-01-01")
-    email: Mapped[str] = mapped_column(unique=True)
-    number_phone: Mapped[str] = mapped_column(unique=True)
-    address_registration: Mapped[str] = mapped_column(nullable=False,server_default="Не определено")
-    source_finance: Mapped[str] = mapped_column(nullable=False, server_default="Не определено")
-    form_study: Mapped[str] = mapped_column(nullable=False, server_default="Не определено")
-    group_id: Mapped[str] = mapped_column(ForeignKey("partitions.study_group.group_id"),unique=True)
-    qualification: Mapped[str] = mapped_column(nullable=False, server_default="Не определено")
-    add_info: Mapped[str]
-    photo_user: Mapped[str]
-
-class AchievementsCategories(Base):
-    __tablename__  = "achievements_categories"
-    __table_args__ = {'schema': 'partitions'}
-    achievement_category_id: Mapped[int] = mapped_column(primary_key=True)
-    achievement_category_name: Mapped[String] = mapped_column(nullable=False)
-
-class AchievementsUser(Base):
-    __tablename__ = "achievements_user"
-    __table_args__ = {'schema': 'partitions'}
-    achievement_id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped["User"] = relationship()
-    kourse: Mapped[int]
-    semestr: Mapped[int]
+# Таблица "EnrollmentDecisions"
+class EnrollmentDecisions(Base):
+    __tablename__ = "enrollment_decisions"
+    __table_args__ = {'schema': 'public'}
+    id_enrollment_decision: Mapped[int] = mapped_column(primary_key=True)
+    id_military_application: Mapped[int] = mapped_column(
+        ForeignKey("public.military_applications.id_military_application"), nullable=False)
+    id_protocol: Mapped[int] = mapped_column(ForeignKey("public.selection_protocols.id_protocol"), nullable=False)
+    decision: Mapped[str] = mapped_column(nullable=False)
+    military_card_issued: Mapped[bool] = mapped_column(default=False)
+    create_dttm: Mapped[datetime] = mapped_column(default=datetime.now, nullable=False)
+    change_dttm: Mapped[datetime] = mapped_column(nullable=True)
+    # Relationships (если нужно)
+    protocol: Mapped["SelectionProtocols"] = relationship(back_populates="enrollment_decisions")
+    military_application: Mapped["MilitaryApplications"] = relationship(back_populates="enrollment_decisions")
--- VUCService/schemas/Schemas.py
+++ VUCService/schemas/Schemas.py
@@ -4,51 +4,121 @@ from typing import List
 from pydantic import BaseModel
 from sqlalchemy import Date
 
+from pydantic import BaseModel, constr, conint, confloat
+from datetime import date, datetime, time
 
-class UserAddSchema(BaseModel):
-    login_user: str
-    password_user: str
-class UserGetSchema(UserAddSchema):
-    pass
+class PositionsInVucSchema(BaseModel):
+    position_in_vuc_id: int
+    position_in_vuc_name: constr(max_length=100) = "Не определено"
+    create_dttm: datetime
+    change_dttm: datetime | None = None
 
-class InstitutiesAddSchema(BaseModel):
-    institute_name: str
+class RoleInNaryadSchema(BaseModel):
+    role_in_naryad_id: int
+    role_name_in_naryad: constr(max_length=100) = "Не определено"
+    create_dttm: datetime
+    change_dttm: datetime | None = None
 
-class InstitutiesGetSchema(InstitutiesAddSchema):
-    institute_id: int
+class VucUsersSchema(BaseModel):
+    vuc_user_id: int
+    position_in_vuc_id: int
+    manager_id: int | None = None
+    last_name: str = "Не определено"
+    first_name: str = "Не определено"
+    middle_name: str = "Не определено"
+    birth_dt: date = date(1925, 1, 1)
+    signal_ind: constr(max_length=8) = "active"
+    create_dttm: datetime
+    change_dttm: datetime | None = None
+    auto_delete_dt: date
 
-class FacultiesAddSchema(BaseModel):
-    faculty_code: str
-    faculty_name: str
-    institute_id: int
+class TempVucUsersInNaryadSchema(BaseModel):
+    naryad_id: int
+    naryad_dt: date = date.today()
+    start_naryad_tm: time
+    end_naryad_tm: time
+    vuc_user_id: int
+    role_in_naryad_id: int
+    signal_ind: constr(max_length=8) = "active"
+    create_dttm: datetime
+    change_dttm: datetime | None = None
+    auto_delete_dt: date
 
-class StudyGroupAddSchema(BaseModel):
-    group_name: str
-    faculty_id: int
+class MilitaryInventorySchema(BaseModel):
+    id_inventory: int
+    name_inventory: constr(max_length=100)
+    category_inventory: constr(max_length=50)
+    quantity_inventory: conint(ge=0)
+    responsible_id: int
+    last_dt_check_inventory: date = date.today()
+    create_dttm: datetime
+    change_dttm: datetime | None = None
 
-class UserInfoAddSchema(BaseModel):
-    user_status: str
-    login_user: str
-    last_name: str
-    first_name: str
-    middle_name: str
-    birth_dt: date
-    email: str
-    number_phone: str
-    address_registration: str
-    source_finance: str
-    form_study: str
-    group_id: int
-    qualification: str
-    add_info: str
-    photo_user: str
+class InventoryTransactionsSchema(BaseModel):
+    id_inventory_transaction: int
+    id_inventory: int
+    type_transaction: constr(max_length=20)
+    amount: conint(gt=0)
+    transaction_date: date = date.today()
+    signal_ind: constr(max_length=8) = "active"
+    issuer_id: int
+    vuc_user_id: int
+    create_dttm: datetime
+    change_dttm: datetime | None = None
 
+class MilitaryApplicationsSchema(BaseModel):
+    id_military_application: int
+    signal_ind: constr(max_length=8) = "active"
+    student_id: int
+    last_name: str = "Не определено"
+    first_name: str = "Не определено"
+    middle_name: str = "Не определено"
+    birth_dt: date = date(1925, 1, 1)
+    study_group_name: str = "Не определено"
+    application_date: date = date.today()
+    status_military_application: constr(max_length=30) = "В процессе"
+    create_dttm: datetime
+    change_dttm: datetime | None = None
 
-class Mark(BaseModel):
-    mark: int
-class TempMarksAdd(BaseModel):
-    last_name: str
-    first_name: str
-    middle_name: str
-    discipline: str
-    List[Mark]: int
+class SelectionProtocolsSchema(BaseModel):
+    id_protocol: int
+    protocol_date: date = date.today()
+    vacancy_count: conint(gt=0)
+    min_rank: confloat(ge=0)
+    id_military_application: int
+    create_dttm: datetime
+    change_dttm: datetime | None = None
+
+class PreliminaryResultsSchema(BaseModel):
+    id_preliminary_result: int
+    id_military_application: int
+    medical_status: constr(max_length=50)
+    psych_test_score: conint(ge=0, le=100)
+    create_dttm: datetime
+    change_dttm: datetime | None = None
+
+class MainResultsSchema(BaseModel):
+    id_main_result: int
+    id_military_application: int
+    gpa: confloat(ge=2.0, le=5.0)
+    physical_score: conint(ge=0, le=100)
+    total_rank: confloat(ge=0)
+    create_dttm: datetime
+    change_dttm: datetime | None = None
+
+class EnrollmentDecisionsSchema(BaseModel):
+    id_enrollment_decision: int
+    id_military_application: int
+    id_protocol: int
+    decision: constr(max_length=20)
+    military_card_issued: bool = False
+    create_dttm: datetime
+    change_dttm: datetime | None = None
+
+class InventoryTransactionsAddSchema(BaseModel):
+    id_inventory: int
+    type_transaction: str
+    amount: int
+    transaction_date: date
+    issuer_id: int
+    vuc_user_id: int
